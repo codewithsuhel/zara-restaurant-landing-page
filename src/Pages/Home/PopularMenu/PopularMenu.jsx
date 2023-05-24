8fd634cd377fb59f4a26b578e0fc89f43a3741c5
unknown <you@example.com>
2023-05-24 15:19:37 +0600

    Navbar added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 
 const PopularMenu = () => {
+
+    useEffect(()=>{
+        fetch(`menu.json`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+    },[])
+    
     return (
          <section>
             <SectionTitle
                 heading={"From Our Menu"}
                 subHeading={"Popular Items"}
-            
             ></SectionTitle>
          </section>
     );
