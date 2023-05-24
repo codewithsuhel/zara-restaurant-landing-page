0adc02014cd420cd9fbcbca56b04d69e1e10e544
unknown <you@example.com>
2023-05-24 16:20:39 +0600

    route decleare

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -19,14 +19,14 @@ const PopularMenu = () => {
     },[])
 
     return (
-         <section>
+         <section className="mb-16 mt-16">
             <SectionTitle
                 heading={"From Our Menu"}
                 subHeading={"Popular Items"}
             ></SectionTitle>
          
 
-         <div>
+         <div className="grid md:grid-cols-2 gap-10">
             {
                 menu.map(item=> <MenuItem
                 key ={item._id}
--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,9 +1,16 @@
 
 
-const MenuItem = () => {
+const MenuItem = ({item}) => {
+    const { image, name, price, recipe } = item;
+
     return (
-        <div>
-            
+        <div className="flex space-x-2">
+            <img style={{borderRadius:'0 200px 200px 200px'}} className="w-[120px]" src={image} alt="" />
+            <div>
+                <h3 className="uppercase">{name} ---------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">${price}</p>
         </div>
     );
 };
