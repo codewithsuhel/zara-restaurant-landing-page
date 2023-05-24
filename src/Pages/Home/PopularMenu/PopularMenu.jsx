fe0ac8d66b820ba91c1b7d6b7409537b498a3eda
unknown <you@example.com>
2023-05-24 15:42:38 +0600

    banner added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 
 const PopularMenu = () => {
 
+    const [menu, setMenu] = useState([])
+
     useEffect(()=>{
         fetch(`menu.json`)
         .then(res=>res.json())
         .then(data=>{
-            console.log(data)
+            // console.log(data);
+            const popularItems = data.filter(item => item.category === 'popular')
+            setMenu(popularItems);
+
         })
     },[])
-    
+
     return (
          <section>
             <SectionTitle
