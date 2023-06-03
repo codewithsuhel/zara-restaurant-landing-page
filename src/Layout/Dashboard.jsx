6f609c686ba3320da3ed6102dfd20f9063d6c417
unknown <you@example.com>
2023-06-04 01:24:45 +0600

    dashboard user added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,13 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaBorderAll, FaEdit, } from 'react-icons/fa';
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaBorderAll, FaShoppingBag, FaBook, FaUtensils, FaUsers, FaAddressBook, } from 'react-icons/fa';
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
   const [cart] = useCart();
+
+  // TODO : load data from the server to have dynamic isAdmin based on data 
+  const isAdmin = true;
+
 
   return (
             <div className="drawer drawer-mobile">
@@ -20,19 +24,37 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80">
 
+                  {
+                    isAdmin ? <>
+                    
+                    <li> <NavLink to='/dashboard/home'><FaHome></FaHome> Admin Home </NavLink> </li>
+                <li> <NavLink to='/dashboard/reservations'> <FaUtensils></FaUtensils> Add Items </NavLink> </li>
+                <li> <NavLink to='/dashboard/history'><FaWallet></FaWallet> Manage Items </NavLink> </li>
+                <li> <NavLink to='/dashboard/history'><FaBook></FaBook> Manage Bookings </NavLink> </li>
+                <li> <NavLink to='/dashboard/allusers'><FaUsers></FaUsers> All Users </NavLink> </li>
+              
+                    
+                  </> : <>
+
+                    <li> <NavLink to='/dashboard/home'><FaHome></FaHome> User Home </NavLink> </li>
+                    <li> <NavLink to='/dashboard/reservations'><FaCalendarAlt></FaCalendarAlt> Reservations </NavLink> </li>
+                   <li> <NavLink to='/dashboard/history'><FaWallet></ FaWallet> Payment History </NavLink> </li>
+                    <li> 
+                    <NavLink to='/dashboard/mycart'><FaShoppingCart></  FaShoppingCart> My Cart <span className="badge  badge-secondary">+{cart?.length || 0}
+                    </span></NavLink>
+                          </li>
+                    
+                    </>
+                  }
+
                  {/* Sidebar content here */}
 
-                <li> <NavLink to='/dashboard/home'><FaHome></FaHome> User Home </NavLink> </li>
-                <li> <NavLink to='/dashboard/reservations'><FaCalendarAlt></FaCalendarAlt> Reservations </NavLink> </li>
-                <li> <NavLink to='/dashboard/history'><FaWallet></FaWallet> Payment History </NavLink> </li>
-                <li> 
-                  <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart> My Cart <span className="badge badge-secondary">+{cart?.length || 0}
-                  </span></NavLink>
-                  </li>
+                
                 <div className="divider"></div>
                 <li> <NavLink to='/'><FaHome></FaHome>Home </NavLink> </li>
                 <li><NavLink to="/menu"> <FaBorderAll></FaBorderAll> Menu</NavLink></li>
-                <li><NavLink to="/order/salad"> <FaEdit></FaEdit> Order Food</NavLink></li>
+                <li><NavLink to="/order/salad"> <FaShoppingBag></FaShoppingBag>  Shop</NavLink></li>
+                <li><NavLink to="Contact"> <FaAddressBook></FaAddressBook>  Contact</NavLink></li>
                 
                 </ul>
             </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import MyCart from "../Pages/Dashboard/MyCart/MyCart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
 
   export const router = createBrowserRouter([
@@ -52,6 +53,10 @@ import MyCart from "../Pages/Dashboard/MyCart/MyCart";
         {
           path:'mycart',
           element:<MyCart></MyCart>
+        },
+        {
+          path:'allusers',
+          element:<AllUsers></AllUsers>
         }
       ]
     }
